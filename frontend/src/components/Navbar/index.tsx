74fe16ea1382186ab0e0fda531c6eb8387ddfec3
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-13 14:35:43 -0300

    ultimo2

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <h1>WHDIAZmovie</h1>
                     <a href="https://github.com/whdiaz" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
