f822e50bb0b2267aa14f9ba90d17c59a423120a8
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-15 21:43:35 -0300

    first request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,13 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+    axios.get(`${BASE_URL}/movies`)
+     .then(response => {
+           console.log(response.data)
+    });
     return (
         <>
             <Pagination />
